7df2188d0bbc4cc53fbc4f5fd010452ee94013b6
Andrea Tupini <tupini07@gmail.com>
2020-02-05 17:45:41 -0600

    simple visitor

--- piper/__main__.py
+++ piper/__main__.py
@@ -1,4 +1,39 @@
+import antlr4
+
+from piper.aparser import dumdumLexer, dumdumParser, dumdumVisitor
+
+from piper import aparser
+
+
+class CustomVisitor(dumdumVisitor.dumdumVisitor):
+    # Visit a parse tree produced by dumdumParser#program.
+    def visitProgram(self, ctx: dumdumParser.):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by dumdumParser#oration.
+    def visitOration(self, ctx:dumdumParser.OrationContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by dumdumParser#expr.
+    def visitExpr(self, ctx:dumdumParser.ExprContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by dumdumParser#method.
+    def visitMethod(self, ctx:dumdumParser.MethodContext):
+        return self.visitChildren(ctx)
+
 
 
 if __name__ == "__main__":
-    pass
+    
+    lexer = dumdumLexer.dumdumLexer(antlr4.FileStream("test.dum"))
+
+    stream = antlr4.CommonTokenStream(lexer)
+
+    parser = dumdumParser.dumdumParser(stream)
+
+    tree = parser.program()
+    import pudb; pudb.set_trace()
