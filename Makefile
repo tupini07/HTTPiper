ac004b518b048bc9ee5af4b982b53316e2967703
Andrea Tupini <tupini07@gmail.com>
2020-02-05 18:15:36 -0600

    basic working example with visitor pattern

--- Makefile
+++ Makefile
@@ -2,8 +2,8 @@ GRAMMAR_NAME = phttp.g4
 
 compile-grammar: 
 	rm -rf piper/aparser
-	antlr4 -Dlanguage=Python3 -visitor -no-listener -o piper/aparser $(GRAMMAR_NAME)
-	touch piper/aparser/__init__.py
+	antlr4 -Dlanguage=Python3 -visitor -o piper/aparser $(GRAMMAR_NAME)
+	# touch piper/aparser/__init__.py
 
 extend-classpath:
 	export CLASSPATH=".:$(shell cat $(shell which antlr4) | grep antlr-complete | cut -d" " -f3):$$CLASSPATH"
--- piper/__main__.py
+++ piper/__main__.py
@@ -2,33 +2,37 @@ import antlr4
 
 from piper.aparser import dumdumLexer, dumdumParser, dumdumVisitor
 
-from piper import aparser
-
 
 class CustomVisitor(dumdumVisitor.dumdumVisitor):
     # Visit a parse tree produced by dumdumParser#program.
-    def visitProgram(self, ctx: dumdumParser.):
-        return self.visitChildren(ctx)
-
+    def visitProgram(self, ctx: dumdumParser.dumdumParser.ProgramContext):
+        return {
+            "full_program": ctx.getText(),
+            "orations": [self.visitOration(x) for x in ctx.oration()],
+        }
 
     # Visit a parse tree produced by dumdumParser#oration.
-    def visitOration(self, ctx:dumdumParser.OrationContext):
-        return self.visitChildren(ctx)
-
+    def visitOration(self, ctx: dumdumParser.dumdumParser.OrationContext):
+        return {
+            "full_oration": ctx.getText(),
+            "expr": self.visitExpr(ctx.expr()),
+        }
 
     # Visit a parse tree produced by dumdumParser#expr.
-    def visitExpr(self, ctx:dumdumParser.ExprContext):
-        return self.visitChildren(ctx)
-
+    def visitExpr(self, ctx: dumdumParser.dumdumParser.ExprContext):
+        return {
+            "full_expression": ctx.getText(),
+            "method": self.visitMethod(ctx.method()),
+            "alpha": ctx.getTokens(dumdumParser.dumdumParser.ALPHA)[0].getText()
+        }
 
     # Visit a parse tree produced by dumdumParser#method.
-    def visitMethod(self, ctx:dumdumParser.MethodContext):
-        return self.visitChildren(ctx)
-
+    def visitMethod(self, ctx: dumdumParser.dumdumParser.MethodContext):
+        return ctx.getText()
 
 
 if __name__ == "__main__":
-    
+
     lexer = dumdumLexer.dumdumLexer(antlr4.FileStream("test.dum"))
 
     stream = antlr4.CommonTokenStream(lexer)
@@ -36,4 +40,4 @@ if __name__ == "__main__":
     parser = dumdumParser.dumdumParser(stream)
 
     tree = parser.program()
-    import pudb; pudb.set_trace()
+    a = CustomVisitor().visit(tree)
