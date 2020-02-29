03e2b74ef81d7759102bdbce35443879aa2bdc03
Andrea Tupini <tupini07@gmail.com>
2020-02-28 21:16:14 -0600

    add documentation for parser structure and enums

--- src/parser/entities.rs
+++ src/parser/entities.rs
@@ -1,23 +1,26 @@
 pub type Program = Vec<ProgramStatement>;
 pub type SubstitutionableContent = Vec<SubstitutionContentParts>;
 
+/// These denote the different parts of a RequestPart
 #[derive(Debug, PartialEq)]
 pub enum RequestResponse {
     Request,
     Response,
 }
 
+/// These denote the different parts of a RequestResponse
 #[derive(Debug, PartialEq)]
 pub enum RequestParts {
     Body,
     Headers,
 }
 
+/// This is the first element in a substitution expression.
+/// It can either be _empty_, pointing to a variable, or pointing to a request.
 #[derive(Debug, PartialEq)]
 pub enum SubstitutionRoot {
     Empty,
     VariableReference(String),
-    NoSubtitution(String),
     RequestReference {
         name: String,
         req_resp: RequestResponse,
@@ -25,18 +28,27 @@ pub enum SubstitutionRoot {
     },
 }
 
+/// A single substitution can have a root followed by zero or more
+/// piped commands
 #[derive(Debug)]
 pub struct SubstitutionDetails {
     pub root: SubstitutionRoot,
     pub commands: Vec<String>,
 }
 
+/// Any content/value can be an composed of multiple parts, each of
+/// which can either be a _substitution_ or plain content (so no substitution
+/// needed).
 #[derive(Debug)]
 pub enum SubstitutionContentParts {
     NoSobstitution(String),
     Substitution(SubstitutionDetails),
 }
 
+/// A program is composed a multiple kinds of statements:
+/// - Import statement
+/// - Variable assignment
+/// - Request definition
 #[derive(Debug)]
 pub enum ProgramStatement {
     ImportFileName(String),
@@ -47,12 +59,16 @@ pub enum ProgramStatement {
     RequestDefinition(RequestDefinition),
 }
 
+/// Represents a single key value pair, which is a header
+/// in a request
 #[derive(Debug)]
 pub struct SingleHeader {
     pub key: String,
     pub value: SubstitutionableContent,
 }
 
+/// This basically represents a JSON object which is the body of
+/// a request. In a body, any _value_ can be substitutable content
 #[derive(Debug)]
 pub enum BodyValues {
     Array(Vec<BodyValues>),
@@ -60,6 +76,7 @@ pub enum BodyValues {
     Terminal(SubstitutionableContent),
 }
 
+/// A struct which represents a single request definition
 #[derive(Debug)]
 pub struct RequestDefinition {
     pub name: String,
